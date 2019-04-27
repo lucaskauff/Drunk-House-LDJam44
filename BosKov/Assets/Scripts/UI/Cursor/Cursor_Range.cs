d579b7893640a197ad7582795a358c438e5b102f
Kilian <allairekilian@gmail.com>
2019-04-27 09:02:23 +0200

    Test avant crash

--- BosKov/Assets/Scripts/UI/Cursor/Cursor_Range.cs
+++ BosKov/Assets/Scripts/UI/Cursor/Cursor_Range.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-namespace BoseKov
+namespace BosKov
 {
     [ExecuteInEditMode]
     public class Cursor_Range : MonoBehaviour
     {
         [SerializeField] Transform curvePoint;
+        [SerializeField] Image yCursor;
         [SerializeField] TextMeshProUGUI yRange;
         [SerializeField] TextMeshProUGUI xRange;
+
+        Vector3 transY;
 
         int yOutput = 0;
         int xOutput = 0;
 
+
         // Start is called before the first frame update
         void Start()
         {
-            yRange = GetComponentInChildren<TextMeshProUGUI>();
-            xRange = GetComponentInChildren<TextMeshProUGUI>();
 
         }
 
@@ -33,11 +35,20 @@ namespace BoseKov
 
         void Y_XRange()
         {
+            transY = yCursor.transform.localPosition;
+            transY.y = curvePoint.localPosition.y;
+            yCursor.transform.localPosition = transY;
+
             yOutput = Mathf.RoundToInt(curvePoint.localPosition.y * 1000);
             yRange.text = yOutput.ToString();
 
             xOutput += Mathf.RoundToInt(Time.deltaTime * 10);
             xRange.text = xOutput.ToString();
+
+            if (curvePoint.localPosition.y > 0)
+            { yRange.color = new Color(0, 1, 0); }
+            else
+            { yRange.color = new Color(1, 0, 0); }
         }
     }
 }
