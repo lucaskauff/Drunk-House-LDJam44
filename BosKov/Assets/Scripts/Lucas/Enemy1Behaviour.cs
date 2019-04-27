1960c1d90f77224192fb055fc0c11d0a77f3b35d
Lucas Kauff <l.kauff@rubika-edu.com>
2019-04-27 07:51:17 +0200

    Barrack Obama in da house

---
+++ BosKov/Assets/Scripts/Lucas/Enemy1Behaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BosKov
+{
+    public class Enemy1Behaviour : MonoBehaviour
+    {
+        [Header("My Components")]
+        [SerializeField] SpriteRenderer mySpriteRend = default;
+        [SerializeField] Collider2D myCollider = default;
+
+        [Header("Serializable")]
+        [SerializeField] float enemySpeed = 1;
+
+        [Header("Public")]
+        public Transform targetPoint = null;
+        public bool isFired = false;
+
+        private bool isFiredCheck = false;
+
+        private void Update()
+        {
+            if (isFired)
+            {
+                if (!isFiredCheck)
+                {
+                    mySpriteRend.enabled = true;
+                    myCollider.enabled = true;
+
+                    isFiredCheck = true;
+                }
+
+                MoveTowardsTarget();
+
+                if (transform.position == targetPoint.position)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        private void MoveTowardsTarget()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, enemySpeed * Time.deltaTime);
+        }
+    }
+}
