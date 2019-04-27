7a5926566308ee3f93574b7c10e40267a59a1efe
Kilian <allairekilian@gmail.com>
2019-04-27 08:01:43 +0200

    UI

---
+++ BosKov/Assets/Scripts/UI/Cursor/Cursor_Range.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace BoseKov
+{
+    [ExecuteInEditMode]
+    public class Cursor_Range : MonoBehaviour
+    {
+        [SerializeField] Transform curvePoint;
+        [SerializeField] TextMeshProUGUI yRange;
+        [SerializeField] TextMeshProUGUI xRange;
+
+        int yOutput = 0;
+        int xOutput = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            yRange = GetComponentInChildren<TextMeshProUGUI>();
+            xRange = GetComponentInChildren<TextMeshProUGUI>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Y_XRange();
+
+        }
+
+        void Y_XRange()
+        {
+            yOutput = Mathf.RoundToInt(curvePoint.localPosition.y * 1000);
+            yRange.text = yOutput.ToString();
+
+            xOutput += Mathf.RoundToInt(Time.deltaTime * 10);
+            xRange.text = xOutput.ToString();
+        }
+    }
+}
