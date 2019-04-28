2cd019b036591de002bafa6369f464175ba641d3
Kilian <allairekilian@gmail.com>
2019-04-29 00:29:32 +0200

    Post pro 100%

--- Boskov2/Assets/Shaders/VHS/VHSPostProcessEffect.cs
+++ Boskov2/Assets/Shaders/VHS/VHSPostProcessEffect.cs
@@ -1,58 +1,79 @@
 using UnityEngine;
 using UnityEngine.Video;
 
-[ExecuteInEditMode]
-[AddComponentMenu("Image Effects/GlitchEffect")]
-[RequireComponent(typeof(Camera))]
-[RequireComponent(typeof(VideoPlayer))]
-public class VHSPostProcessEffect : MonoBehaviour
+namespace Boskov
 {
-    public Shader shader;
-    public VideoClip VHSClip;
+    [AddComponentMenu("Image Effects/GlitchEffect")]
+    [RequireComponent(typeof(Camera))]
+    [RequireComponent(typeof(VideoPlayer))]
+    public class VHSPostProcessEffect : MonoBehaviour
+    {
+        [SerializeField] private GameCoreData gameCore;
 
-    private float _yScanline;
-    private float _xScanline;
-    [SerializeField] [Range (0 ,1)] private int _IsBlacked;
-    private Material _material = null;
-	private VideoPlayer _player;
+        public Shader shader;
+        public VideoClip VHSClip;
 
-	void Start()
-	{
-		_material = new Material(shader);
-		_player = GetComponent<VideoPlayer>();
-		_player.isLooping = true;
-		_player.renderMode = VideoRenderMode.APIOnly;
-		_player.audioOutputMode = VideoAudioOutputMode.None;
-		_player.clip = VHSClip;
-		_player.Play();
-	}
+        private float _yScanline;
+        private float _xScanline;
+        [SerializeField] [Range(0, 1)] private int _IsBlacked;
+        private Material _material = null;
+        private VideoPlayer _player;
 
-	void OnRenderImage(RenderTexture source, RenderTexture destination)
-	{
-		_material.SetTexture("_VHSTex", _player.texture);
+        void Start()
+        {
+            _material = new Material(shader);
+            _player = GetComponent<VideoPlayer>();
+            _player.isLooping = true;
+            _player.renderMode = VideoRenderMode.APIOnly;
+            _player.audioOutputMode = VideoAudioOutputMode.None;
+            _player.clip = VHSClip;
+            _player.Play();
+        }
 
-		_yScanline += Time.deltaTime * 0.01f;
-		_xScanline -= Time.deltaTime * 0.1f;
+        private void Update()
+        {
+            PowerData();
+        }
 
-		if (_yScanline >= 1)
-		{
-			_yScanline = Random.value;
-		}
-		if (_xScanline <= 0 || Random.value < 0.05)
-		{
-			_xScanline = Random.value;
-		}
-		_material.SetFloat("_yScanline", _yScanline);
-		_material.SetFloat("_xScanline", _xScanline);
-        _material.SetFloat("_IsBlacked", _IsBlacked);
-        Graphics.Blit(source, destination, _material);
-	}
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            _material.SetTexture("_VHSTex", _player.texture);
 
-	protected void OnDisable()
-	{
-		if (_material)
-		{
-			DestroyImmediate(_material);
-		}
-	}
+            _yScanline += Time.deltaTime * 0.01f;
+            _xScanline -= Time.deltaTime * 0.1f;
+
+            if (_yScanline >= 1)
+            {
+                _yScanline = Random.value;
+            }
+            if (_xScanline <= 0 || Random.value < 0.05)
+            {
+                _xScanline = Random.value;
+            }
+            _material.SetFloat("_yScanline", _yScanline);
+            _material.SetFloat("_xScanline", _xScanline);
+            _material.SetFloat("_IsBlacked", _IsBlacked);
+            Graphics.Blit(source, destination, _material);
+        }
+
+        protected void OnDisable()
+        {
+            if (_material)
+            {
+                DestroyImmediate(_material);
+            }
+        }
+
+        private void PowerData()
+        {
+            if(gameCore.VladimirState.energy.current <= 0)
+            {
+                _IsBlacked = 1;
+            }
+            else
+            {
+                _IsBlacked = 0;
+            }
+        }
+    }
 }
