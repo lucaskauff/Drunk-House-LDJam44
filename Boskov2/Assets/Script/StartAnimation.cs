fc3a1f3ab37631add1aba37aaf307e1458b67d39
Unknown <c.pavie@rubika-edu.com>
2019-04-29 01:51:47 +0200

    GameOver Menu

---
+++ Boskov2/Assets/Script/StartAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour
+{
+    [SerializeField] GameObject gameAnim;
+    [SerializeField] GameObject overAnim;
+    [SerializeField] float speed;
+
+    private NormalDistribution test = new NormalDistribution(-1, -115, 50);
+
+
+    private void Update()
+    {
+        float value = test.GetValueFrom(gameAnim.transform.localPosition.x) +1.030f;
+        gameAnim.transform.Translate(Vector2.right * value * speed);
+        overAnim.transform.Translate(Vector2.left * value * speed);
+    }
+
+}
